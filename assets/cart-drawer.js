895870e95f9b8102998d096cbab067bbb8cb6f39
ONORA <contactmilaura.souffle@gmail.com>
2026-08-07 07:44:56 +0200

    fix: redesign empty cart drawer

--- assets/cart-drawer.js
+++ assets/cart-drawer.js
@@ -118,9 +118,7 @@ class CartDrawer extends HTMLElement {
     this.addEventListener(
       'transitionend',
       () => {
-        const containerToTrapFocusOn = this.classList.contains('is-empty')
-          ? this.querySelector('.drawer__inner-empty')
-          : document.getElementById('CartDrawer');
+        const containerToTrapFocusOn = this.querySelector('.drawer__inner');
         const focusElement = this.querySelector('.drawer__inner') || this.querySelector('.drawer__close');
         trapFocus(containerToTrapFocusOn, focusElement);
       },
